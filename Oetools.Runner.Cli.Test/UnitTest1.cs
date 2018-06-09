2eccdd20ebda691485c5dcf73522c07e8c18902d
Julien Caillon <julien.caillon@gmail.com>
2018-06-09 14:44:29 +0200

    not working yet

---
+++ Oetools.Runner.Cli.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Oetools.Runner.Cli.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
