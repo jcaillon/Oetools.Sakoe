4406f51381fabd0fae653ddd8ab735684fa38e14
Julien Caillon <julien.caillon@gmail.com>
2018-06-25 20:37:42 +0200

    dotnet + net461 build

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@ setlocal enabledelayedexpansion
 
 REM https://github.com/Microsoft/msbuild/wiki/MSBuild-Tips-&-Tricks
 REM msbuild.cmd Oetools.Runner.Cli /p:targetFrameworks="net461" /p:VersionPrefix=1.0.1 /p:VersionSuffix= /p:FileVersion=1.0.1.789 /p:ProductVersion=1.0.1 /p:Configuration=Release /p:Platform="Any CPU" /p:IncludeSymbols=true /verbosity:minimal /t:Restore,Rebuild /m
-msbuild.cmd Oetools.Runner.Cli /p:targetFrameworks="net461" /p:Version="1.0.1" /p:Configuration=Release /p:Platform="Any CPU" /p:IncludeSymbols=true /verbosity:minimal /t:Restore,Rebuild /m
 
-dotnet publish Oetools.Runner.Cli\Oetools.Runner.Cli.csproj --framework netcoreapp2.0 --configuration Release /p:Version="1.0.1" /p:Platform="Any CPU" /p:IncludeSymbols=true /verbosity:minimal /m /bl:msbuild.binlog
+dotnet publish Oetools.Runner.Cli\Oetools.Runner.Cli.csproj --framework netcoreapp2.0 --configuration Release /p:Version="1.0.1" /p:Platform="Any CPU" /p:IncludeSymbols=true /verbosity:minimal /m /bl:dotnet.binlog
+
+msbuild.cmd Oetools.Runner.Cli\Oetools.Runner.Cli.csproj /p:targetFrameworks="net461" /p:Version="1.0.1" /p:Configuration=Release /p:Platform="Any CPU" /p:IncludeSymbols=true /verbosity:minimal /t:Restore,Rebuild /m
