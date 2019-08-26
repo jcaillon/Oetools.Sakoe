b3e3a80897773c18bc7948ad9f4fba3613fd14ba
Julien Caillon <julien.caillon@gmail.com>
2019-08-26 16:18:41 +0200

    up SoftwareUpdater

--- build.ps1
+++ build.ps1
@@ -11,16 +11,17 @@ param (
 
 function Main {
     # inspired by ci script from https://github.com/datalust/piggy.
-	# inspired by ci script from https://github.com/Azure/azure-functions-core-tools.
-    $path = $ProjectOrSolutionPath	
-	[string] $ciTag = If ([string]::IsNullOrEmpty($env:CI_COMMIT_TAG)) {$env:CI_COMMIT_TAG} Else {$env:APPVEYOR_REPO_TAG_NAME}
-    $isReleaseBuild = [string]::IsNullOrEmpty($ciTag)
+    # inspired by ci script from https://github.com/Azure/azure-functions-core-tools.
+    $path = $ProjectOrSolutionPath  
+
+    [string] $ciTag = If ([string]::IsNullOrEmpty($env:APPVEYOR_REPO_TAG_NAME)) {$env:CI_COMMIT_TAG} Else {$env:APPVEYOR_REPO_TAG_NAME}
+    $isReleaseBuild = ![string]::IsNullOrEmpty($ciTag)
     [string] $versionToBuild = $NULL
     if ($isReleaseBuild) {
-        $versionToBuild = If ($ciTag.StartsWith('v')) {$ciTag.SubString(1)} Else {$env:ciTag}
+        $versionToBuild = If ($ciTag.StartsWith('v')) {$ciTag.SubString(1)} Else {$ciTag}
     }
 
-	Write-Host "Building $(If ([string]::IsNullOrEmpty($versionToBuild)) { "default version" } Else { " version $versionToBuild" } )"
+    Write-Host "Building $(If ([string]::IsNullOrEmpty($versionToBuild)) { "default version" } Else { "version $versionToBuild" } )"
 
 	Push-Location $PSScriptRoot
 	if ($UpdateDocumentationOnly) {
